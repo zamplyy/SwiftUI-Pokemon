aa3eed78f77d58dba0a7662bcf9cc0d92e586ce4
Joar Karlsson <joar.karlsson@evry.com>
2020-10-05 09:48:42 +0200

    Initial Commit

---
+++ PokemonAppGrid/PokemonAppGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppGridApp.swift
+//  PokemonAppGrid
+//
+//  Created by Joar Karlsson on 2020-10-05.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
